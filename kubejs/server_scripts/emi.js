258f73694cf4ef5a525c51b4d8140cea1f939da5
ShadowzOfTheDark <57125303+ShadowzOfTheDark@users.noreply.github.com>
2026-07-16 20:41:13 -0500

    Well map recipe, fix immersive enchanting for update.

--- kubejs/server_scripts/emi.js
+++ kubejs/server_scripts/emi.js
@@ -15,5 +15,5 @@ RecipeViewerEvents.removeEntries('fluid', event => {
 // GuideME
 RecipeViewerEvents.addEntries('item', event => {
     event.add('guideme:guide[guideme:guide_id="breakingnewground:guide"]')
-
+    event.add('supplementaries:cartographers_quill[supplementaries:quill_structure="malum:weeping_well",custom_name=\'"Sub-terranian Arcane Anomaly"\',supplementaries:quill_search_radius=1000,supplementaries:quill_decoration="supplementaries:waystone",supplementaries:quill_color=-1634752001]')
 })
