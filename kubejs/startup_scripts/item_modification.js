2caef6b31090975701d2faa3278be83b259e96e9
ShadowzOfTheDark <57125303+ShadowzOfTheDark@users.noreply.github.com>
2026-08-25 20:52:21 -0500

    Make diamond veins more rich

--- kubejs/startup_scripts/item_modification.js
+++ kubejs/startup_scripts/item_modification.js
@@ -9,5 +9,28 @@ ItemEvents.modification(event => {
       item.maxDamage = item.get('minecraft:max_damage') * 3
     }
   })
+
+  event.modify('architects_palette:entrails', item => {
+    item.modifyFood(food => {
+        food.alwaysEdible(true)
+        food.nutrition(4)
+        food.saturation(0.5)
+        food.eatSeconds(0.2)
+        food.effect("minecraft:hunger",300,79,1)
+        food.effect("minecraft:slowness",100,0,1)
+        food.effect("minecraft:nausea",80,0,0.2)
+        food.effect("minecraft:blindness",80,0,0.2)
+    })
+  })
+
+  event.modify('architects_palette:rotten_flesh_block', item => {
+    item.modifyFood(food => {
+        food.nutrition(2)
+        food.saturation(0.5)
+        food.eatSeconds(0.25)
+        food.effect("minecraft:hunger",200,79,1)
+        food.effect("minecraft:nausea",80,0,0.2)
+    })
+  })
   
 })
