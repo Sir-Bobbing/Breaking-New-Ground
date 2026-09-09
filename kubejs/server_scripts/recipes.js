aef34bfd54a6971407aff8e728231676be0e5a21
ShadowzOfTheDark <57125303+ShadowzOfTheDark@users.noreply.github.com>
2026-09-08 20:03:01 -0500

    Fix issue #105

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,35 @@
+ServerEvents.recipes(event => {
+
+    event.remove({ mod: "copycats", not: { output: ["copycats:copycat_box", "copycats:copycat_catwalk"] } })
+    event.remove({ output: "create:copycat_panel" })
+
+    function addCopycatRecipes(list, count) {
+        let countString = count + 'x '
+        for (let id of list) {
+            let countId = countString + id
+            event.stonecutting(countId, 'create:zinc_ingot')
+
+            event.shapeless(Item.of('create:zinc_ingot'),[countId])
+        }
+    }
+
+    addCopycatRecipes([
+        "copycats:copycat_block",
+        "copycats:copycat_ghost_block",
+        "copycats:copycat_beam",
+        "copycats:copycat_slope",
+        "create:copycat_panel",
+        "copycats:copycat_door",
+        "copycats:copycat_trapdoor",
+        "copycats:copycat_fence_gate",
+        "copycats:copycat_ladder"
+    ], 4)
+
+    addCopycatRecipes([
+        "copycats:copycat_layer",
+        "copycats:copycat_board",
+        "copycats:copycat_wooden_pressure_plate",
+        "copycats:copycat_wooden_button",
+    ], 8)
+
+})
